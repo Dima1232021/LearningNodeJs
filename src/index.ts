0a6b7807d64757650dd8ee7244741b4d53d4f6e8
Dima1232021 <77456284+Dima1232021@users.noreply.github.com>
2024-11-04 22:20:37 +0200

    added cors (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
@@ -8,6 +9,7 @@ import { SignUpSchema } from "./schema/users";
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', rootRouter);
 
